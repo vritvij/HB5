0e5155611a935e8800eb7bd9b4d4936394542954
Niranjan <meniranjan2309@gmail.com>
2018-04-14 16:44:56 -0700

    Beacon Android App

---
+++ Beacon/app/src/main/java/com/example/menir/beacon/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.menir.beacon;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button sosButtonVar = (Button) findViewById(R.id.sosButton);
+
+        sosButtonVar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TextView t  = (TextView) findViewById(R.id.textView);
+                //t.setText("Hello");
+
+                if(sosButtonVar.getText().toString().equals("SOS")){
+
+                    sosButtonVar.setText("Cancel");
+                }
+                else if(sosButtonVar.getText().toString().equals("Cancel")){
+                    sosButtonVar.setText("SOS");
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- hb5-beacon/app/src/main/java/com/example/ninad/hb5_/MainActivity.java
+++ hb5-beacon/app/src/main/java/com/example/ninad/hb5_/MainActivity.java
@@ -2,35 +2,80 @@ package com.example.ninad.hb5_;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
+import android.location.Location;
 import com.uriio.beacons.Beacons;
 import com.uriio.beacons.model.Beacon;
+import com.uriio.beacons.model.EddystoneEID;
+import com.uriio.beacons.model.EddystoneTLM;
 import com.uriio.beacons.model.EddystoneURL;
+import com.uriio.beacons.model.iBeacon;
+
+import android.location.Location;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button sosButton;
+    GPSTracker gps;
+    Beacon first;
+    Beacon second;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Beacons.initialize(this);
-        final Beacon abc = new EddystoneURL("https://github.com");
+
         setContentView(R.layout.activity_main);
-        final Button sosButton = (Button) findViewById(R.id.sos_button);
+        sosButton = (Button) findViewById(R.id.sosButton);
+        //abc = new EddystoneTLM(6000,"Hello_Niranjan");
+        first = new EddystoneURL("Lat");
+        second = new EddystoneURL("Long");
+        first.start();
+        second.start();
+
+
         sosButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String text = (String)sosButton.getText();
-                if(text.equals("Enable")){
-                    abc.start();
-                    sosButton.setText("Disable");
+                if(sosButton.getText().equals("SOS")) {
+                    gps = new GPSTracker(MainActivity.this);
+                    // check if GPS enabled
+                    if (gps.canGetLocation()) {
+                        double latitude=0.0;
+                        double longitude=0.0;
+                        String url_lat;
+                        String url_lng;
+                        sosButton.setText("Cancel");
+                        while(true) {
+
+                            if(latitude!=gps.getLatitude() && longitude!=gps.getLongitude()){
+                                latitude = gps.getLatitude();
+                                longitude = gps.getLongitude();
+                                first.stop();
+                                second.stop();
+                                url_lat = "https://" + latitude;
+                                url_lng = "http://" + longitude;
+                                first = new EddystoneURL(url_lat);
+                                second = new EddystoneURL(url_lng);
+
+                                first.start();
+                                second.start();
+                            }
+                            //Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+                        }
+                    } else {
+                        gps.showSettingsAlert();
+                    }
                 }
                 else{
-                    abc.stop();
-                    sosButton.setText("Enable");
+                    gps.stopUsingGPS();
+                    first.stop();
+                    second.stop();
+                    Toast.makeText(getApplicationContext(), "GPS is Stopped", Toast.LENGTH_LONG).show();
+                    sosButton.setText("SOS");
                 }
-
             }
         });
     }
